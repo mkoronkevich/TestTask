8f5a346e2e2d5e66dfc25df186bdf726031ee6d3
Milana Koronkevich <milana.koronkevich@tut.by>
2015-09-16 21:38:28 +0300

    Initial Commit

---
+++ Generator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Generator
+//
+//  Created by Milana Koronkevich on 9/16/15.
+//  Copyright (c) 2015 Milana Koronkevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Generator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Generator
+//
+//  Created by Milana Koronkevich on 9/16/15.
+//  Copyright (c) 2015 Milana Koronkevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
